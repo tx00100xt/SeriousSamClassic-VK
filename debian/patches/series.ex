a252d8a042d8dbb3597eb2d68e661d1f46a10acb
tx00100xt <t.x00100x.t@yandex.ru>
2023-10-16 22:19:35 +0300

    Removed deprecated debian patches. Update changelog

--- debian/patches/series.ex
+++
@@ -1,8 +0,0 @@
-0001-remove-SE1_10b-depend.patch
-0002-cmakeFind.patch
-0003-mainCMakeLists.patch
-0004-SamTFE-CMakeLists.patch
-0005-SamTSE-CMakeLists.patch
-0006-Fix-Install-Scripts.patch
-0007-Fixed_Platform_definition.patch
-0008-Fix-load-some-incorrect-custom-maps.patch
