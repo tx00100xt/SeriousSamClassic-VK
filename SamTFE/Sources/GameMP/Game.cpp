08a2a348914e4577547d28abb7e096b44becc889
tx00100xt <t.x00100x.t@yandex.ru>
2023-10-20 22:43:47 +0300

    Textures are not drawn on some levels. fix #31

--- SamTFE/Sources/GameMP/Game.cpp
+++ SamTFE/Sources/GameMP/Game.cpp
@@ -298,6 +298,11 @@ void _FixTexturesLuxor(void)
     pl = CPlacement3D(FLOAT3D(m_fCoord1, m_fCoord2, m_fCoord3), ANGLE3D(0, 0, 0));
     _CreateLights(pl, 8.0f);
   }
+  FLOAT m_fCoord1 = _fLuxorCoordinates[51][0];
+  FLOAT m_fCoord2 = _fLuxorCoordinates[51][1];
+  FLOAT m_fCoord3 = _fLuxorCoordinates[51][2];
+  pl = CPlacement3D(FLOAT3D(m_fCoord1, m_fCoord2, m_fCoord3), ANGLE3D(0, 0, 0));
+  _CreateLights(pl, 1.0f);
 }
 
 void _FixTexturesSacredYards(void) 
@@ -450,7 +455,7 @@ void MaybeDiscardLastLines(void)
       _FixTexturesAlleyOfSphinxes();
     } else if ( strModName=="" && strLevelName=="12_Karnak") {
       _FixTexturesKarnak();
-    } else if ( strModName=="" && strLevelName=="13_Luxor") {
+    } else if (/* strModName=="" && */ strLevelName=="13_Luxor") {
       _FixTexturesLuxor();
     } else if ( strModName=="" && strLevelName=="14_SacredYards") {
       _FixTexturesSacredYards();
